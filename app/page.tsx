7eaac95622b87e97239c9ebc196d3bc85cf1cb1c
aay7ush <aay7ush@gmail.com>
2024-03-26 00:22:44 +0530

    setup shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
 	return (
-		<h1 className="text-3xl font-bold underline">Let's build YouTube Clone</h1>
+		<>
+			<h1 className="text-3xl font-bold underline">
+				Let's build YouTube Clone
+			</h1>
+			<Button>Click Me</Button>
+		</>
 	)
 }
